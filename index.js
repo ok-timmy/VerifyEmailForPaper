8d2f9b3c63513e78d5680223ea7f6e180e7f7f51
Timilehin Okunola <okunolatimilehin4123@gmail.com>
2023-05-30 19:07:25 +0100

    Made Changes

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ app.use(express.json());
 
 app.use(cors());
 app.use("/", verifyEmailRoute);
+app.get("/", (req, res)=>{
+  res.json({message: " This works, thank God"});
+})
 
 app.listen(5000, () => {
   console.log(`Example app listening on port 5000`);
