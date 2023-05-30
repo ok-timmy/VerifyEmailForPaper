92fcf822b832f70ba42a6cbd2fe6679e1f6606d7
Timilehin Okunola <okunolatimilehin4123@gmail.com>
2023-05-30 20:17:43 +0100

    Made changes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 // const verifyEmailRoute = require("./Routes/verifyEmail");
 const cors = require("cors");
 const dotenv = require("dotenv")
+const axios = require("axios");
 
 dotenv.config();
 
